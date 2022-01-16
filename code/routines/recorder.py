fb0a70422dff42cbc81d68b35457556c221e2635
Ruoff Oliver (XC-AD/ECS2) <oliver.ruoff@de.bosch.com>
2022-01-16 12:45:40 +0100

    added finer movements

--- code/routines/recorder.py
+++ code/routines/recorder.py
@@ -22,5 +22,25 @@ def record(left_stepper, right_stepper):
             right_stepper.set_direction(False)
             left_stepper.turn_stepper_angle(207, True)
             right_stepper.turn_stepper_angle(207, True)
+        elif input_ == "u":
+            left_stepper.set_direction(False)
+            right_stepper.set_direction(True)
+            left_stepper.turn_stepper_angle(100, True)
+            right_stepper.turn_stepper_angle(100, True)
+        elif input_ == "j":
+            left_stepper.set_direction(True)
+            right_stepper.set_direction(False)
+            left_stepper.turn_stepper_angle(100, True)
+            right_stepper.turn_stepper_angle(100, True)
+        elif input_ == "h":
+            left_stepper.set_direction(True)
+            right_stepper.set_direction(True)
+            left_stepper.turn_stepper_angle(57, True)
+            right_stepper.turn_stepper_angle(57, True)
+        elif input_ == "k":
+            left_stepper.set_direction(False)
+            right_stepper.set_direction(False)
+            left_stepper.turn_stepper_angle(57, True)
+            right_stepper.turn_stepper_angle(57, True)
         left_stepper.activate_stepper()
         right_stepper.activate_stepper()
