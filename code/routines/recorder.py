36b79e03bb9f9fd664147d0a92b205cc4a595f85
Ruoff Oliver (XC-AD/ECS2) <oliver.ruoff@de.bosch.com>
2022-01-16 12:35:22 +0100

    fix user input colliding with variable

--- code/routines/recorder.py
+++ code/routines/recorder.py
@@ -1,23 +1,23 @@
 def record(left_stepper, right_stepper):
-    input = ""
-    while input is not "z":
-        input = input("Move >> ")
-        if input == "w":
+    input__ = ""
+    while input_ is not "z":
+        input_ = input("Move >> ")
+        if input_ == "w":
             left_stepper.set_direction(False)
             right_stepper.set_direction(True)
             left_stepper.turn_stepper_angle(360, True)
             right_stepper.turn_stepper_angle(360, True)
-        elif input == "s":
+        elif input_ == "s":
             left_stepper.set_direction(True)
             right_stepper.set_direction(False)
             left_stepper.turn_stepper_angle(360, True)
             right_stepper.turn_stepper_angle(360, True)
-        elif input == "a":
+        elif input_ == "a":
             left_stepper.set_direction(True)
             right_stepper.set_direction(True)
             left_stepper.turn_stepper_angle(207, True)
             right_stepper.turn_stepper_angle(207, True)
-        elif input == "d":
+        elif input_ == "d":
             left_stepper.set_direction(False)
             right_stepper.set_direction(False)
             left_stepper.turn_stepper_angle(207, True)
