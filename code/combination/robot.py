8c5dc5ffa06fedbef2b5a336aa3eab67adb38365
Ruoff Oliver (XC-AD/ECS2) <oliver.ruoff@de.bosch.com>
2022-01-17 19:20:02 +0100

    added robot turning with angle

--- code/combination/robot.py
+++ code/combination/robot.py
@@ -20,8 +20,17 @@ class Robot:
             self.right_stepper.set_direction_clockwise(False)
         desired_angle = (cm / self.TYRE_CIRCUMFERENCE_CM) * 360
         self.left_stepper.turn_stepper_angle(desired_angle, True)
-        self.right_stepper.turn_stepper_angle(desired_angle, True)
+        self.right_stepper.turn_stepper_angle(desired_angle, False)
 
     def turn_degree(self, degree, clockwise):
-        self.left_stepper.turn_stepper_angle(828, True)
-        self.right_stepper.turn_stepper_angle(828, True)
+        if clockwise:
+            self.left_stepper.set_direction_clockwise(False)
+            self.right_stepper.set_direction_clockwise(False)
+        else:
+            self.left_stepper.set_direction_clockwise(True)
+            self.right_stepper.set_direction_clockwise(True)
+
+        desired_angle = (self.ROBOT_CIRCUMFERENCE_CM /
+                         self.TYRE_CIRCUMFERENCE_CM) * degree
+        self.left_stepper.turn_stepper_angle(desired_angle, True)
+        self.right_stepper.turn_stepper_angle(desired_angle, False)
