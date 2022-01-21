7aad4be566c7f084008fb54c983730cf4a3ac95a
Ruoff Oliver (XC-AD/ECS2) <oliver.ruoff@de.bosch.com>
2022-01-21 22:20:35 +0100

    reset stepper position after lidar 360 scan

--- code/lidar/lidar.py
+++ code/lidar/lidar.py
@@ -15,7 +15,7 @@ def scan_360(stepper, tfluna, clockwise=True):
     env_map = _scan_360(stepper, tfluna, clockwise)
     reverse = not clockwise
     stepper.set_direction_clockwise(reverse)
-    stepper.run_stepper(200, False)
+    stepper.turn_stepper_angle(360, halfstep=False)
     return env_map
 
 
