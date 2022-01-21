7653bf54650cbef7cfacd74ffb8da860cb9fd26f
Ruoff Oliver (XC-AD/ECS2) <oliver.ruoff@de.bosch.com>
2022-01-21 20:33:49 +0100

    Update ULN2003 to also support full steps

--- code/actuators/ULN2003.py
+++ code/actuators/ULN2003.py
@@ -1,3 +1,4 @@
+from msilib import sequence
 import RPi.GPIO as GPIO
 import time
 
@@ -11,6 +12,13 @@ HALFSTEP_SEQ = [
     [0, 0, 1, 1],
     [0, 0, 0, 1],
     [1, 0, 0, 1]
+]
+
+FULLSTEP_SEQ = [
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1]
 ]
 
 
@@ -31,22 +39,30 @@ class ULN2003:
         self.direction_clockwise = True
         self.steps_per_revolution = steps_per_revolution
 
-    def run_stepper(self, steps, stepper_delay=0.001):
+    def run_stepper(self, steps, stepper_delay=0.001, halfstep=True):
         """Runs the stepper motor for the given number of steps.
         Note, that "clockwise" depends on pin setup.
-        512 steps == 1 round.
+        512 steps == 1 round for smaller, 200 steps == 1 round for bigger stepper.
         Arguments:
             steps {int} -- Number of steps the motor will do.
         Keyword Arguments:
             stepper_delay {float} -- Defines the time to wait between steps. (default: {0.001})
+            halfstep {bool} -- Defines wheather to use half steps or fullsteps (default: {True})
         """
-        halfstep_range = range(
-            8) if not self.direction_clockwise else list(reversed(range(8)))
+
+        if halfstep:
+            seq = HALFSTEP_SEQ
+            step_range = range(
+                8) if not self.direction_clockwise else list(reversed(range(8)))
+        else:
+            seq = FULLSTEP_SEQ
+            step_range = range(
+                4) if not self.direction_clockwise else list(reversed(range(4)))
         for _ in range(steps):
-            for halfstep in halfstep_range:
+            for step in step_range:
                 for pin in range(4):
                     GPIO.output(
-                        self.stepper_pins[pin], HALFSTEP_SEQ[halfstep][pin])
+                        self.stepper_pins[pin], seq[step][pin])
                 time.sleep(stepper_delay)
 
     def set_direction_clockwise(self, clockwise):
@@ -57,10 +73,14 @@ class ULN2003:
         """
         self.direction_clockwise = clockwise
 
-    def turn_stepper_angle(self, angle_in_degree):
+    def turn_stepper_angle(self, angle_in_degree, halfstep=True):
         """This function turns the stepper motor to a specific angle in degree.
 
         Args:
             angle_in_degree (int): Angle in degree, on how much the stepper will be turned.
+        Keyword Arguments:
+            halfstep {bool} -- Defines wheather to use half steps or fullsteps (default: {True})
         """
+        divisor = self.steps_per_revolution * \
+            2 if halfstep else self.steps_per_revolution
         self.run_stepper(int(self.steps_per_revolution/360 * angle_in_degree))
