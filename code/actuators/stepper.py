6c340ef6f4241303bd7faf6b50fd6979443b3cc1
Ruoff Oliver (XC-AD/ECS2) <oliver.ruoff@de.bosch.com>
2022-02-09 20:21:23 +0100

    appl

--- code/actuators/stepper.py
+++ code/actuators/stepper.py
@@ -135,10 +135,10 @@ class stepper:
         Returns:
             float: The resulting y value, which can be used as stepper delay in seconds.
         """
-        X_AXIS_SHIFT = -0.008
+        X_AXIS_SHIFT = -0.006
         GRAPH_WIDTH = abs(2*X_AXIS_SHIFT)
         EXPONENT = 2
-        PARABOLA_SHARPNESS = 15000
+        PARABOLA_SHARPNESS = 1, 000, 000
         x = GRAPH_WIDTH/all_steps*current_step
         y = PARABOLA_SHARPNESS*pow(x+X_AXIS_SHIFT, EXPONENT) * \
             self.stepper_delay_seconds + self.stepper_delay_seconds
