d181a9f579dc2b0bba4541568cb9a28b7f747407
Ruoff Oliver (XC-AD/ECS2) <oliver.ruoff@de.bosch.com>
2022-02-25 19:40:09 +0100

    added log

--- code/main.py
+++ code/main.py
@@ -73,6 +73,6 @@ while True:
         print(cloud_2)
     elif a == "12":
         cloud_1 = robo.lidar.scan_angle_with_stepper_position_reset(360)
-        slam_.score_2d_clouds(cloud_1, cloud_1)
+        print(slam_.score_2d_clouds(cloud_1, cloud_1))
     else:
         print("Command not recognized!")
